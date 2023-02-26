c5e75375d6692a6c31b8601846cab0c3b8a63a0f
Kisekio <dekostump@gmail.com>
2023-02-26 18:29:49 -0500

    Creacion de constructores

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Deko Academy", 2020);
+            escuela.Pais= "Colombia";
+            escuela.Ciudad= "Bogota";
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
